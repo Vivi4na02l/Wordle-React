e882c6ca6c29bcaf87819756238c2d6acfc0380a
Viviana <40200339@esmad.ipp.pt>
2026-05-07 18:01:41 +0100

    added API to get a random word

--- src/Components/Wordle.jsx
+++ src/Components/Wordle.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react'
 import WordsBoard from './WordsBroad.jsx'
 import Keyboard from'./Keyboard.jsx'
 
 export default function Wordle() {
+    const [randomWord, setRandomWord] = useState("");
+    
+    useEffect(() => {
+        async function getWord() {
+            const randomLength = Math.floor(Math.random() * 3)+4;
+
+            const response = await fetch(`https://random-words-api.kushcreates.com/api?words=1&language=en&category=animals&length=${randomLength}`);
+
+            const data = await response.json();
+            setRandomWord(data[0].word);
+
+            console.log(data);        
+        }
+
+        getWord();
+    }, [])
+
     return (
         <main>
+            <h2>{randomWord}</h2>
             <WordsBoard />
             <Keyboard />
         </main>
