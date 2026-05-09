cf263f6711911c3ff14d9522b8746767472b8d55
Viviana <40200339@esmad.ipp.pt>
2026-05-09 11:14:35 +0100

    New API added to check if the word written by user exists or not.
    Added logic that lets the user guess a new word if the previous word existed, and the opposite if not.

--- src/Components/Wordle.jsx
+++ src/Components/Wordle.jsx
@@ -52,13 +52,52 @@ export default function Wordle() {
         })
     }
 
-    function enterClicked() {
+    /**
+     * checks if the word written exists (!) and if it does, saves it and moves user to the next line to write more words
+     */
+    async function enterClicked() {
         const guessedWord = guessingWord.join("");
-        setGuessedWords(prevGuessedWords => [...prevGuessedWords, guessedWord]) //pushes guessedWord to the array
 
-        console.log(guessedWords);
-        setNbrWordsGuessed(nbrPrev => nbrPrev +1)
-        setGuessingWord([])
+        const exists = await validateGuessedWord(guessedWord);
+
+        if (exists == true) {
+            console.log("word exists");
+
+            setGuessedWords(prevGuessedWords => [...prevGuessedWords, guessedWord]) //pushes guessedWord to the array
+            setNbrWordsGuessed(nbrPrev => nbrPrev +1) //moves on to the next line
+            setGuessingWord([]) //restarts the value that stores the word being currently written
+        } else {
+            console.log("word doesn't exist");   
+        }
+    }
+
+    /**
+     * api that checks if the word written by the user exists
+     * @param {*} word 
+     */
+    async function validateGuessedWord(word) {
+        try {
+            const response = await fetch(
+                `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+            );
+
+            if (!response.ok) {
+                const doesNotExistData = await response.json();
+                console.log(doesNotExistData);
+                // word doesn't exist
+                return false
+            }
+
+            // word exists
+            const data = await response.json();
+            console.log(data);
+            return true
+        }
+
+        catch (error) {
+            console.log('error: '+error);
+            return false
+        }
     }
 
     return (
